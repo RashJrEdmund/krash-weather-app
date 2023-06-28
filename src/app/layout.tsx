ab4fa173998ca04d37184d8ff6a60c6568d1645a
rash <orashusedmund@gmail.com>
2023-06-28 06:48:38 +0100

    added the meta tag to allow insecure sites

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
   return (
     <WeatherContextProvider>
       <html lang="en">
+        <head>
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />
+        </head>
         <body className={inter.className}>{children}</body>
       </html>
     </WeatherContextProvider>
