127daa2bba6331d7d1909661a426454a7b0a0211
rash <orashusedmund@gmail.com>
2023-07-01 13:23:40 +0100

    solved the overload erro

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -11,7 +11,7 @@ export const getDay = (date: any) => {
   // date.toLocaleString(undefined, {....}) looks like : 'Saturday, 1 July 2023'
   // now you get why i'm spliting at new Date().getFullYear() and taking the first val
 
-  const year = new Date().getFullYear();
+  const year: any = new Date().getFullYear();
 
   const res = date
     .toLocaleString(undefined, {
